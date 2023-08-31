01120c8cab324c1147f945fb8ce6ac89565ab0e7
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-08-30 21:43:17 -0500

    Group pages imports

---
+++ src/app/modules/products/presentation/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './add-product-page/add-product-page.component';
+export * from './edit-product-page/edit-product-page.component';
+export * from './product-list-page/product-list-page.component';
