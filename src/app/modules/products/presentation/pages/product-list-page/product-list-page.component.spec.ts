2bd0887006bc5cc1bd4dd607e3eb9154da2f3e38
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-08-31 23:09:13 -0500

    Add product list page implementation according to acceptance criteria

--- src/app/modules/products/presentation/pages/product-list-page/product-list-page.component.spec.ts
+++ src/app/modules/products/presentation/pages/product-list-page/product-list-page.component.spec.ts
@@ -1,21 +1,136 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { firstValueFrom, of } from 'rxjs';
 
 import { ProductListPageComponent } from './product-list-page.component';
+import { ProductsService } from '../../../domain/services/products-service';
+import { ProductMother } from '../../../domain/mothers';
 
 describe('ProductListPageComponent', () => {
   let component: ProductListPageComponent;
   let fixture: ComponentFixture<ProductListPageComponent>;
+  let productsService: any;
 
   beforeEach(() => {
+    productsService = jasmine.createSpyObj(['getAll']);
     TestBed.configureTestingModule({
       declarations: [ProductListPageComponent],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: productsService,
+        },
+      ],
     });
-    fixture = TestBed.createComponent(ProductListPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture = TestBed.createComponent(ProductListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should set the default values', waitForAsync(() => {
+    productsService.getAll.and.returnValue(of([ProductMother.random()]));
+    fixture = TestBed.createComponent(ProductListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    // The initial page size is 5
+    component.pageSize$.subscribe((pageSize) => {
+      expect(pageSize).toBe(5);
+    });
+
+    // The initial page is 1
+    component.currentPage$.subscribe((currentPage) => {
+      expect(currentPage).toBe(1);
+    });
+
+    // The initial total pages is 1
+    component.totalPages$.subscribe((totalPages) => {
+      expect(totalPages).toBe(1);
+    });
+  }));
+
+  it('should render the list of products', waitForAsync(() => {
+    const products = ProductMother.randomList(7);
+    productsService.getAll.and.returnValue(of(products));
+
+    fixture = TestBed.createComponent(ProductListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    // All products (total)
+    component.allProducts$.subscribe((products) => {
+      expect(products.length).toBe(7);
+    });
+
+    // Page products (5 per page)
+    component.pageProducts$.subscribe((products) => {
+      expect(products.length).toBe(5);
+    });
+  }));
+
+  it('should filter the products by name', waitForAsync(async () => {
+    const firstProduct = ProductMother.random();
+    firstProduct.name = 'First';
+    const secondProduct = ProductMother.random();
+    secondProduct.name = 'Second';
+
+    productsService.getAll.and.returnValue(of([firstProduct, secondProduct]));
+    fixture = TestBed.createComponent(ProductListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component.searchValue$.next('First');
+
+    // Only the first product should be returned
+    const pageProducts = await firstValueFrom(component.pageProducts$);
+    expect(pageProducts.length).toBe(1);
+  }));
+
+  it('should calculate the total pages', waitForAsync(async () => {
+    const products = ProductMother.randomList(12);
+    productsService.getAll.and.returnValue(of(products));
+
+    fixture = TestBed.createComponent(ProductListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    // Initial total pages is 3 (5 per page)
+    const firstTotalPages = await firstValueFrom(component.totalPages$);
+    expect(firstTotalPages).toBe(3);
+
+    component.pageSize$.next(10);
+
+    // Total pages is 2 (10 per page)
+    component.totalPages$.subscribe((totalPages) => {
+      expect(totalPages).toBe(2);
+    });
+  }));
+
+  it('should change the page products when next or previous page is clicked', waitForAsync(async () => {
+    const products = ProductMother.randomList(7);
+    productsService.getAll.and.returnValue(of(products));
+
+    fixture = TestBed.createComponent(ProductListPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    // Initial products (page 1)
+    const products1 = await firstValueFrom(component.pageProducts$);
+    expect(products1.length).toBe(5);
+
+    component.onNextPage();
+
+    // Next page products (page 2)
+    const products2 = await firstValueFrom(component.pageProducts$);
+    expect(products2.length).toBe(2);
+
+    component.onPreviousPage();
+
+    // Previous page products (page 1)
+    const products3 = await firstValueFrom(component.pageProducts$);
+    expect(products3.length).toBe(5);
+  }));
 });
--- src/app/modules/products/presentation/pages/product-list-page/product-list-page.component.ts
+++ src/app/modules/products/presentation/pages/product-list-page/product-list-page.component.ts
@@ -1,8 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  Subscription,
+} from 'rxjs';
+
+import { ProductsService } from '../../../domain/services/products-service';
+import { ProductItem } from '../../../domain/models/product';
 
 @Component({
   selector: 'app-product-list-page',
   templateUrl: './product-list-page.component.html',
   styleUrls: ['./product-list-page.component.css'],
 })
-export class ProductListPageComponent {}
+export class ProductListPageComponent implements OnInit, OnDestroy {
+  allProducts$ = this.productsService.getAll();
+  searchValue$ = new BehaviorSubject('');
+  currentPage$ = new BehaviorSubject(1);
+  totalPages$: Observable<number> = of(0);
+  pageSize$ = new BehaviorSubject(5);
+  pageProducts$: Observable<ProductItem[]> = of([]);
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    @Inject(ProductsService)
+    private readonly productsService: ProductsService,
+  ) {}
+
+  ngOnInit(): void {
+    const searchValue$ = this.searchValue$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+    );
+
+    const productsOrSearchValueChange$ = combineLatest([
+      this.allProducts$,
+      searchValue$,
+    ]);
+
+    this.subscriptions.push(
+      productsOrSearchValueChange$.subscribe(() => {
+        this.currentPage$.next(1);
+      }),
+    );
+
+    const filteredProducts$ = productsOrSearchValueChange$.pipe(
+      map(([products, searchValue]) => {
+        return products.filter((product) =>
+          product.name.toLowerCase().includes(searchValue.toLowerCase()),
+        );
+      }),
+    );
+
+    this.pageProducts$ = combineLatest([
+      filteredProducts$,
+      this.currentPage$,
+      this.pageSize$,
+    ]).pipe(
+      map(([filteredProducts, page, pageSize]) => {
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+        return filteredProducts.slice(startIndex, endIndex);
+      }),
+    );
+
+    this.totalPages$ = combineLatest([filteredProducts$, this.pageSize$]).pipe(
+      map(([products, pageSize]) => Math.ceil(products.length / pageSize)),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  onInput(value: string) {
+    this.searchValue$.next(value);
+  }
+
+  onNextPage() {
+    this.currentPage$.next(this.currentPage$.value + 1);
+  }
+
+  onPreviousPage() {
+    this.currentPage$.next(this.currentPage$.value - 1);
+  }
+
+  onPageSizeChange(pageSize: string) {
+    this.pageSize$.next(Number(pageSize));
+  }
+}
