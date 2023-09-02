222a79d0fbc63b31557921d1194ccebf062c54e9
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-09-01 23:35:15 -0500

    Remove unused component

--- src/app/modules/products/presentation/pages/edit-product-page/edit-product-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-edit-product-page',
-  templateUrl: './edit-product-page.component.html',
-  styleUrls: ['./edit-product-page.component.css'],
-})
-export class EditProductPageComponent {}
