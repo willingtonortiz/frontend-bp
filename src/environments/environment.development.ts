f7c2906ed85e5feb703b89c9be38fde5292137f3
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-09-01 23:35:31 -0500

    Add authorId environment variable

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   productsApiUrl:
     'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros',
+  authorId: '252135325',
 };
