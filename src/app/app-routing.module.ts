cb30d1f610bd86b15ca216fd850f5b859dc7e3af
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-09-02 00:38:46 -0500

    Update edit product route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListPageComponent } from './modules/products/presentation/pages/product-list-page/product-list-page.component';
 import { AddProductPageComponent } from './modules/products/presentation/pages/add-product-page/add-product-page.component';
-import { EditProductPageComponent } from './modules/products/presentation/pages/edit-product-page/edit-product-page.component';
 
 const routes: Routes = [
   {
@@ -15,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'edit/:id',
-    component: EditProductPageComponent,
+    component: AddProductPageComponent,
   },
 ];
 
