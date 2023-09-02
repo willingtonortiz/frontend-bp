12c1ff658d855f389b3181d681e52d41d1c63e7b
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-09-01 23:36:24 -0500

    Remove unused import

--- src/app/modules/products/presentation/pages/index.ts
+++ src/app/modules/products/presentation/pages/index.ts
@@ -1,3 +1,2 @@
 export * from './add-product-page/add-product-page.component';
-export * from './edit-product-page/edit-product-page.component';
 export * from './product-list-page/product-list-page.component';
